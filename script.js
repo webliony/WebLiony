f0d965d1f9beef83c062dc1cfcccebe77a150f02
WebLiony <163657750+webliony@users.noreply.github.com>
2024-03-18 15:45:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,33 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+
+  const images = ["images/slide4.jpg","images/slide7.jpg","images/slide9.jpg"];
+let currentIndex = 0;
+
+// Function to change image source
+function changeImage() {
+    const img = document.getElementById('responsiveImage');
+    currentIndex = (currentIndex + 1) % images.length;
+    img.src = images[currentIndex];
+}
+
+// Change image every 3 seconds
+setInterval(changeImage, 3000);
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyAcNfL2_Yv812a0wGb7adOnWPmNS4uMZutqOg5YQE8Yfl81W0d0Rpn9GGtph_2nRVw/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
